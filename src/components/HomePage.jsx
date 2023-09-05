6c13f42c9b04fbe74b08dc13c86482d43d108641
Michael Mathew <michaelmathew34@gmail.com>
2023-09-05 09:36:05 +0100

    added favicon, updated image changing function

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,22 +1,14 @@
-import React, { useEffect, useState } from "react";
-import mobile from '../assets/images/illustration-sign-up-mobile.svg';
-import desktop from '../assets/images/illustration-sign-up-desktop.svg'
+import React from "react";
 import Content from "./Content";
+import '../App.css'
+
 
 function HomePage({ handleClick, inputVal }) {
-
-    const [isMobile, setMobile] = useState(false)
-
-    useEffect(() => {
-        if (window.screen.width <= 770) {
-            setMobile(true);
-        }
-    }, []);
 
     return (
         <div className="lg:bg-charcoalGray lg:px-7 lg:justify-center lg:h-screen lg:flex lg:items-center">
             <div className="max-w-full lg:flex lg:flex-row-reverse lg:bg-white md:rounded-3xl lg:p-7 lg:h-[85%] xl:w-[56%] lg:justify-between">
-                <img src={isMobile ? mobile : desktop} alt="mobile design illustration" className="w-full lg:w-auto" />
+                <img alt="design illustration" className="w-full lg:w-auto bg-image" />
                 <Content handleClick={handleClick} inputVal={inputVal} />
             </div>
         </div>
